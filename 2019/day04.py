03caee61869a982710f2c4b4081e5dd17ac638da
Zach Nelson <zach@zdnelson.com>
2019-12-06 12:10:07 -0600

    Second star for 2019 day 4

--- 2019/day04.py
+++ 2019/day04.py
@@ -17,15 +17,44 @@ def is_valid_pwd(pwd, enforce_range=True):
             return False
     return char_match
 
+print(is_valid_pwd(111111, enforce_range=False), 'expected True')
+print(is_valid_pwd(223450, enforce_range=False), 'expected False')
+print(is_valid_pwd(123789, enforce_range=False), 'expected False')
+
 count = 0
 valid_pwds = []
-
-print(is_valid_pwd(111111, enforce_range=False))
-print(is_valid_pwd(223450, enforce_range=False))
-print(is_valid_pwd(123789, enforce_range=False))
-
 for pwd in range(1000000):
     if is_valid_pwd(pwd):
+        count += 1
+        valid_pwds.append(pwd)
+
+print(count, valid_pwds)
+
+def is_valid_pwd_2(pwd, enforce_range=True):
+    if not is_valid_pwd(pwd, enforce_range):
+        return False
+    pwd_s = str(pwd).zfill(6)
+    c = pwd_s[0]
+    repeat_lengths = []
+    length = 1
+    for i in range(1, 6):
+        if pwd_s[i] == c:
+            length += 1
+        else:
+            repeat_lengths.append(length)
+            length = 1
+            c = pwd_s[i]
+    repeat_lengths.append(length)
+    return repeat_lengths.count(2) > 0
+
+print(is_valid_pwd_2(112233, enforce_range=False), 'expected True')
+print(is_valid_pwd_2(123444, enforce_range=False), 'expected False')
+print(is_valid_pwd_2(111122, enforce_range=False), 'expected True')
+
+count = 0
+valid_pwds = []
+for pwd in range(1000000):
+    if is_valid_pwd_2(pwd):
         count += 1
         valid_pwds.append(pwd)
 
